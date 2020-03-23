1d0d6328995a9a1b0d32d7af364191d883d668fe
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-03-23 13:17:19 -0300

    feat: add basic components

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return(
+    <>
+        <aside className="main-sidebar sidebar-dark-primary elevation-4">
+          {/* Brand Logo */}
+          <a href="index3.html" className="brand-link">
+            <span className="brand-text font-weight-light">School</span>
+          </a>
+          {/* Sidebar */}
+          <div className="sidebar">
+            {/* Sidebar user panel (optional) */}
+            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div className="info">
+                <a href="#" className="d-block">Tony Augusto</a>
+              </div>
+            </div>
+            {/* Sidebar Menu */}
+            <nav className="mt-2">
+              <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                {/* Add icons to the links using the .nav-icon class
+                with font-awesome or any other icon font library */}
+                <li className="nav-item has-treeview menu-open">
+                  <a href="#" className="nav-link active">
+                    <i className="nav-icon fas" />
+                    <p>
+                      Professores
+                      <i className="right fas fa-angle-left" />
+                    </p>
+                  </a>
+                  <ul className="nav nav-treeview">
+                    <li className="nav-item">
+                      <Link to={"/teachers"} className="nav-link">
+                        <i className="far nav-icon" />
+                        <p>Listar todos</p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={"/teachers/new"} className="nav-link">
+                        <i className="far nav-icon" />
+                        <p>Cadastrar</p>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item has-treeview menu-open">
+                  <a href="#" className="nav-link active">
+                    <i className="nav-icon fas" />
+                    <p>
+                      Cursos
+                      <i className="right fas fa-angle-left" />
+                    </p>
+                  </a>
+                  <ul className="nav nav-treeview">
+                    <li className="nav-item">
+                      <Link to={"/courses"} className="nav-link">
+                        <i className="far nav-icon" />
+                        <p>Listar todos</p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={"/course/new"} className="nav-link">
+                        <i className="far nav-icon" />
+                        <p>Cadastrar</p>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            {/* /.sidebar-menu */}
+          </div>
+          {/* /.sidebar */}
+      </aside>
+    </>
+  );
+}
+
+export default Sidebar;
