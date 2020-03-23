a309598031151422e78b9d743bc56fac11d84d4f
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-03-23 16:52:00 -0300

    refactor: create separated buttons components

---
+++ src/components/Buttons/ButtonPencilEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+  return(
+    <>
+      <a className="btn btn-outline-info btn-sm mr-2" href="#">
+      <i className="fas fa-pencil-alt">
+      </i>
+      Editar
+      </a>
+    </>
+  );
+}
---
+++ src/components/Buttons/ButtonTrashDelete.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+  return(
+    <>
+      <a className="btn btn-outline-danger btn-sm" href="#">
+        <i className="fas fa-trash">
+        </i>
+        Excluir
+      </a>
+    </>
+  );
+}
